380829f9dca59fbdaf19262643b7cec7742e5bed
Rowell Heria <rowell.heria@gmail.com>
2019-12-13 18:03:17 +0000

    Updated demo app to use a http client

--- src/pages/dashboard/dashboard-layout.ts
+++ src/pages/dashboard/dashboard-layout.ts
@@ -1,11 +1,25 @@
+import { autoinject, computedFrom } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-http-client';
 import { IAttendanceRow } from './components/attendance-table';
-import { attendances } from './table-data';
 
+@autoinject
 export class DashboardLayout {
+  constructor(private http: HttpClient) { }
+
+  private attendances: IAttendanceRow[] = [];
+
+  public async attached() {
+    const response = await this.http.get('/api/attendances.json');
+
+    this.attendances = response.isSuccess 
+      ? response.content 
+      : [];
+  }
+
   private get dataset(): Chart.ChartDataSets[] {
     const colours = ['#FFC312', '#C4E538', '#12CBC4', '#FDA7DF', '#ED4C67'];
 
-    return attendances.map((d, i) => {
+    return this.attendances.map((d, i) => {
       const data: Chart.ChartDataSets = {
         label: d.groupName,
         data: [...d.attendances],
@@ -18,18 +32,26 @@ export class DashboardLayout {
     });
   }
 
-  public tableData: IAttendanceRow[] = [...attendances];
+  @computedFrom('attendances')
+  public get tableData(): IAttendanceRow[] {
+    return [...this.attendances];
+  }
 
-  public lineData: Chart.ChartData = {
-    labels: ['Sept', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb'],
-    datasets: this.dataset,
-  };
+  @computedFrom('attendances')
+  public get lineData(): Chart.ChartData {
+    return {
+      labels: ['Sept', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb'],
+      datasets: this.dataset,
+    };
+  }
 
-  public lineOptions: Chart.ChartOptions = {
-    legend: {
-      display: true,
-      position: 'bottom',
-    },
-    maintainAspectRatio: false,
-  };
+  public get lineOptions(): Chart.ChartOptions {
+    return {
+      legend: {
+        display: true,
+        position: 'bottom',
+      },
+      maintainAspectRatio: false,
+    };
+  }
 }
